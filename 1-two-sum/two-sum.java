0e251cc4f19df8c8916bb5838175f718e683bbae
Kirana dh <167740348+Kiran8088dh@users.noreply.github.com>
2025-10-09 18:53:16 +0530

    Time: 2 ms (98.94%) | Memory: 45 MB (65.91%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                    if(nums[i]+nums[j]==target){
-                return new int[]{i,j};
-            }
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+        int complex=target-nums[i];
+        if(map.containsKey(complex)){
+            return new int[] {map.get(complex),i};
+        }else{
+             map.put(nums[i],i);
         }
-            }
-        
-        return new int[]{};
+            
+        }
+        throw new IllegalArgumentException("No Maches");   
     }
 }
